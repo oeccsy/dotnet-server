3dd58c7727fb98543ce1779083a4a71f9df97ae5
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-23 21:54:47 +0900

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
