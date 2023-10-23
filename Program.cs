5ed6cae57f007d9b1dc6f9850f298afe8425f268
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-24 01:22:19 +0900

    feat : add home

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -13,5 +14,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run("http://0.0.0.0:5000");
